eb7fa98c09bb28d1e84700cd75482cd71d7aef5d
Jordan Glassman <jordanglassman@gmail.com>
2015-12-04 23:38:20 -0500

    initial work with CS downloader, mostly javamail tests

--- src/main/java/com/jordanglassman/xdl/XwordDownloader.java
+++ src/main/java/com/jordanglassman/xdl/XwordDownloader.java
@@ -23,13 +23,6 @@ public class XwordDownloader {
 	}
 
 	public static void main(final String[] args) {
-
-//				System.setProperty("org.apache.commons.logging.Log", "org.apache.commons.logging.impl.SimpleLog");
-//				System.setProperty("org.apache.commons.logging.simplelog.showdatetime", "true");
-//				System.setProperty("org.apache.commons.logging.simplelog.log.org.apache.http", "DEBUG");
-//				System.setProperty("org.apache.commons.logging.simplelog.log.org.apache.http.wire", "DEBUG");
-
-
 		final XwordDownloader xdl = new XwordDownloader();
 
 		// do certain things only if we are in dev
